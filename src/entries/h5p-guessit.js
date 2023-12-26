a39473ce50a3cf4a25401e525b46497be2f25da5
rezeau <joseph@rezeau.org>
2023-12-26 23:45:21 +0100

    progress ok except css not minified
    Signed-off-by: rezeau <joseph@rezeau.org>

---
+++ src/entries/h5p-guessit.js
@@ -0,0 +1,8 @@
+import "../styles/guessit.css";
+import "../scripts/guessit-blanks.js";
+import "../scripts/guessit-counter.js";
+import "../scripts/guessit-timer.js";
+
+// Load library
+//H5P = H5P || {};
+//H5P.GuessIt = GuessIt;
